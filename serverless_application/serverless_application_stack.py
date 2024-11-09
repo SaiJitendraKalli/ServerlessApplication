ac53fd5f6bbdc3865ba8e6730b01a06e183d0075
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2024-11-09 15:03:36 +0530

    Initial commit

--- serverless_application/serverless_application_stack.py
+++ serverless_application/serverless_application_stack.py
@@ -1,19 +1,53 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,  # Import the Lambda module
+    aws_iam as _iam,
+aws_logs as _logs,
+    Duration
 )
 from constructs import Construct
+
 
 class ServerlessApplicationStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        lambda_role = _iam.Role(
+            self, "LambdaRole",
+            role_name="CdkLambdaRole",
+            description="Role for Lambda to access services",
+            assumed_by=_iam.ServicePrincipal("lambda.amazonaws.com")
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ServerlessApplicationQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Add permissions to the role
+        lambda_role.add_to_policy(
+            _iam.PolicyStatement(
+                actions=["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+                resources=["arn:aws:logs:*:*:*"]
+            )
+        )
+        #log group for Lambda with retention of 1 day
+        log_group = _logs.LogGroup(
+            self,
+            "Lambda Log Group",
+            log_group_name="/aws/lambda/CdkHelloWorldFunction",
+            retention=_logs.RetentionDays.ONE_DAY
+        )
+
+        # Define the Lambda function resource
+        my_function = _lambda.Function(
+            self, "HelloWorldFunction",
+            runtime=_lambda.Runtime.PYTHON_3_9,  # Provide any supported Node.js runtime
+            handler="index.handler",
+            function_name="CdkHelloWorldFunction",
+            code=_lambda.Code.from_inline("""def handler(event,context):
+            return {
+                "status" : 200
+            }"""
+                                          ),
+            timeout=Duration.seconds(900),  # Timeout in seconds
+            memory_size=128,  # Memory in MBs
+            role=lambda_role,
+            log_group=log_group
+        )

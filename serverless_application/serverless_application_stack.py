d6f0264abd37db7c5cd04ca17d3b4275f7f7c091
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2024-11-09 20:52:57 +0530

    Creating DynamoDB Table

--- serverless_application/serverless_application_stack.py
+++ serverless_application/serverless_application_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     aws_lambda as _lambda,  # Import the Lambda module
     aws_iam as _iam,
     aws_logs as _logs,
+    aws_dynamodb as _dynamodb,
     Duration, RemovalPolicy
 )
 from constructs import Construct
@@ -13,11 +14,22 @@ class ServerlessApplicationStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        dynamo_db_table = _dynamodb.Table(
+            self, "CdkDynamoDbTable",
+            table_name="SampleTable",
+            partition_key=_dynamodb.Attribute(
+                name="id",
+                type=_dynamodb.AttributeType.STRING
+            ),
+            billing_mode=_dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY
+        )
+
         lambda_role = _iam.Role(
             self, "LambdaRole",
             role_name="CdkLambdaRole",
             description="Role for Lambda to access services",
-            #add assume role for DynamoDb and Lambda
+            # add assume role for DynamoDb and Lambda
             assumed_by=_iam.CompositePrincipal(
                 _iam.ServicePrincipal("lambda.amazonaws.com"),
                 _iam.ServicePrincipal("dynamodb.amazonaws.com")
@@ -35,7 +47,7 @@ class ServerlessApplicationStack(Stack):
         #         resources=["arn:aws:logs:*:*:*"]
         #     )
         # )
-        #log group for Lambda with retention of 1 day and delete log group when stack deletes
+        # log group for Lambda with retention of 1 day and delete log group when stack deletes
         log_group = _logs.LogGroup(
             self, "LambdaLogGroup",
             log_group_name="CdkLambdaLogGroup",
@@ -53,5 +65,8 @@ class ServerlessApplicationStack(Stack):
             timeout=Duration.seconds(900),  # Timeout in seconds
             memory_size=128,  # Memory in MBs
             role=lambda_role,
+            environment={
+                "TABLE_NAME": dynamo_db_table.table_name
+            },
             log_group=log_group
         )
